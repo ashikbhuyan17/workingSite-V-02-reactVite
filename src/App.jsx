709c12109d861732e2cd87a1c8ef403502adcf95
unknown <ashikbhuyan.swe.diu@gmail.com>
2023-02-22 05:36:05 +0600

    mock file edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Layout from './layout/Layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CRUD from './pages/crud-operation-raw/CRUD.jsx';
-import Employee from './pages/manage-employees/employee';
+import Employee from './pages/manage-employees/Employee';
 
 export default function App() {
   return (
